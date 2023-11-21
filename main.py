95ef1fb402bd3470ad02a88895b03b9ab47bcb6f
DemirYumruk1 <demircraft5@gmail.com>
2023-11-21 12:26:13 +0000

    started the work
    oh yeah, it's all coming together

---
+++ main.py
@@ -0,0 +1,7 @@
+import pygame
+from maze import Maze
+
+width = int(input("enter width :"))
+height = int(input("enter height :"))
+
+maze = Maze(width, height)
---
+++ maze.py
@@ -0,0 +1,11 @@
+import pygame
+
+class Maze:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.grid = [[0 for x in range(width)] for x in range(height)]
+
+    def run_game_loop():
+        pass
