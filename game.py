70e3554837bc12afe03e0a1313223250c69caeee
DemirYumruk1 <demircraft5@gmail.com>
2023-12-04 12:27:14 +0000

    Added a main menu
    There is now a main menu. Papyrus is also present in the main menu to provide error dialouge. The menu stays open and can restart the maze.
    In the future, it may be possible to change Papyrus' expression depending on state of the game.

--- game.py
+++ game.py
@@ -16,9 +16,9 @@ class Game:
             "YELLOW": 7,
             "BLUE_ELEC": 8 #blue tiles next to yellow tiles will be converted to this tile, do not generate
         }
-        #tiles are 8x8 so the window must be 8 times the amount of tiles to display them all
-        self.window_width = maze_width * 8
-        self.window_height = maze_height * 8
+        #tiles are 16x16 so the window must be 16 times the amount of tiles to display them all
+        self.window_width = maze_width * 16
+        self.window_height = maze_height * 16
 
         #game window size must be based on tiles
         self.game_window = pygame.display.set_mode((self.window_width,self.window_height))
