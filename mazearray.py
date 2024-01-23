8af0ed45e1b93c57b3a90862b4c9b697d7365b20
DemirYumruk1 <demircraft5@gmail.com>
2024-01-23 13:23:37 +0000

    Temporary algorithm complete!
    Temporary algorithm for creating a maze array. Creates a path with no unsafe tiles, then fills the rest of the maze with a selection of tiles biased toward red/yellow as they were excluded from the previous distribution, in an attempt to even out the numbers.

--- mazearray.py
+++ mazearray.py
@@ -19,20 +19,39 @@ class MazeArray:
             "BLUE_ELEC": 8 #blue tiles next to yellow tiles will be converted to this tile, do not generate
         }
 
-    def generateMaze(self):
-        pass
+    def fillMaze(self):
+        for x in range(len(self.grid)):
+            for y in range(len(self.grid[x])):
+                if self.grid[x][y] == "#":
+                    self.grid[x][y] = self.chooseTile(False)
+                    #newtile = triangularly distributed number idk
+                    #self.setTile(i, j, newtile)
+                    pass
+
+    def setTile(self, x, y, input):
+        self.grid[x][y] = input
+
+    def chooseTile(self, safe):
+        if safe:
+            return random.choices(range(1,6))
+        else:
+            return random.choices(range(1,8), weights=(0.1, 0.1, 0.1, 0.1, 0.1, 0.3, 0.3)) 
+        #red/yellow is 3x more likely with "unsafe" generation, this is because red/yellow tiles are not generated during path generation.
+
     def generatePath(self):
         random.seed = self.seed
         #start halfway down the maze, at x = 0, because it is player spawn point
         y = round(((self.height)/2))
         x = 1
-        print(x)
-        print(y)
-        while x != (self.width + 1): #+1 is because the loop should terminate after the last tile is filled
+        #print(x)
+        #print(y)
+        while x != (self.width): #+1 removed, thought it would end loop after filling, caused overflow, reverted
             direction = 0
-            self.grid[x][y] = random.randint(1,5)
+            #print(x)
+            #print(y)
+            self.setTile(x, y, self.chooseTile(True))
             axis = random.choice(["x", "x", "y"])   #2/3 chance of changing x
-            print(axis)
+            #print(axis)
             if axis == "x":
                 direction = random.choice([-1, -1, 1, 1, 1])    #weighting the algorithm to the right to be less prone to walking into itself
                 if x == 0:
@@ -42,7 +61,7 @@ class MazeArray:
                 else:
                     direction *= -1
                     x += direction
-                print(x)
+                #print(x)
             if axis == "y":
                 direction = random.choice([-1, 1])
                 if y == self.height:
@@ -50,11 +69,15 @@ class MazeArray:
                 if y == 0:
                     direction = 1
                 y += direction
-                print(y)
+                #print(y)
             #check if that tile is occupied (grid[x][y] != 0)
             #if occupied, multiply direction by -1
+            #note: algorithm seems to start from coordinates (x,0) [somewhere from the bottom]  instead of (0,y/2) [halfway down, at the first column] for reasons i cannot decipher. 
+            #This algorithm only exists to generate test cases so fixing this is low-priority
 
-if __name__ == "__main__":
-    maze = MazeArray(10,10,100) #test case
+if __name__ == "__main__": #used for testing
+    maze = MazeArray(10,10,255) 
     maze.generatePath()
-    print(maze.grid)
+    maze.fillMaze()
+    for row in maze.grid:
+        print(*row, sep="\t")
