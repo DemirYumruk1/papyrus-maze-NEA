4605751123ba86602bbf57ea62c51178d5f34ccd
DemirYumruk1 <demircraft5@gmail.com>
2024-02-07 18:40:38 +0000

    player file created
    this will be AweSome

---
+++ player.py
@@ -0,0 +1,14 @@
+from game import Game
+class Player:
+    def __init__(self, x, y, sprite, status):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.status = status
+
+    def Move(self):
+        pass
+
+    def setStatus(self):
+        pass
+    
