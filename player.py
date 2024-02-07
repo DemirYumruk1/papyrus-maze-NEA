e4bb64a4c122e7bf8cdf6742345175cdff8d2172
DemirYumruk1 <demircraft5@gmail.com>
2024-02-07 19:27:37 +0000

    made a full player class
    get & set for flavour
    can change sprite based on file path (used to indicate flavour)

--- player.py
+++ player.py
@@ -1,9 +1,11 @@
 import pygame
 class Player:
-    def __init__(self, maze, x, y, sprite, flavour):
+    def __init__(self, x, y, sprite_path, sprite_width, flavour):
         self.x = x
         self.y = y
-        self.sprite = sprite
+        sprite = pygame.image.load(sprite_path)
+        self.sprite_width = sprite_width # only need width, sprite should be 1:1
+        self.sprite = pygame.transform.scale(sprite, (sprite_width, sprite_width))
         self.flavour = flavour
 
     def move(self, direction_y, direction_x, max_y, max_x):
@@ -21,9 +23,15 @@ class Player:
             if direction_x == -1:
                 return
 
-    def setStatus(self, tile):
-        pass
+    def setFlavour(self, flavour):
+        self.flavour = flavour
 
     def getFlavour(self):
         return self.flavour
+
+    def setSprite(self, sprite_path):
+        sprite = pygame.image.load(sprite_path)
+        self.sprite = pygame.transform.scale(sprite, (self.sprite_width, self.sprite_width))
+
+
     
