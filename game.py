673c9e1f95e450061459caf0b56f5741dd942cf9
DemirYumruk1 <demircraft5@gmail.com>
2024-02-28 23:13:23 +0000

    GUI headroom + green fix + solution path revamp
    quite alot i forgot to commit

--- game.py
+++ game.py
@@ -3,6 +3,7 @@ from mazearray import MazeArray
 from player import Player
 class Game:
     def __init__(self, maze_width, maze_height, seed):
+        pygame.init()
         self.seed = seed
         self.maze = MazeArray(maze_width, maze_height, seed)
         self.maze_width = maze_width + 1 #room for the goal tiles
@@ -26,25 +27,33 @@ class Game:
         self.window_width = maze_width * self.tile_width
         self.window_height = maze_height * self.tile_width
         #game window size must be based on tiles
-        self.game_window = pygame.display.set_mode((self.window_width,self.window_height))
+        self.game_window = pygame.display.set_mode((self.window_width, self.window_height + 100)) #+100 gives headroom for GUI
 
         #the player's sprite must also be proportional to the tile size
         self.Player = Player(0, round(maze_height/2), "Assets/player_normal.png", self.tile_width, False)
 
+        self.seconds = 0 #goes up every second (100 ticks)
         self.score = 0 #goes up every time a green tile is pressed
+
+        self.GUIfont = pygame.font.SysFont("comicsansms", 72)
+        self.score_disp = self.GUIfont.render(f"score: {self.score}", True, (0,0,0))
+        self.timer_disp = self.GUIfont.render(f"time: {(self.seconds//60):02}m:{(self.seconds % 60):02}", True, (0,0,0))
+
 
     def draw(self):
         self.game_window.fill(0) #refresh screen
+
         player_displayed_pos_x = self.Player.x * self.tile_width
         player_displayed_pos_y = self.Player.y * self.tile_width
+
 
         for j, row in enumerate(self.mazeArray):
             for i, tile in enumerate(row):
                 colour = self.tile_id[tile[0]] #tile[0], because the tile itself contains a list ([1] instead of 1)
                 pygame.draw.rect(self.game_window, colour, (i * self.tile_width, j * self.tile_width, self.tile_width, self.tile_width))
         self.game_window.blit(self.Player.sprite, (player_displayed_pos_x, player_displayed_pos_y))
-        
-        
+        self.game_window.blit(self.score_disp, (5, self.window_height + 50))
+
         pygame.display.update()
 
     def check_tile(self, dir_x, dir_y):
@@ -57,9 +66,8 @@ class Game:
 
     def run_game_loop(self):
         #initialise game
-        pygame.init()
+        
         clock = pygame.time.Clock()
-
         self.maze.generatePath()
         self.maze.fillMaze()
         player_direction_x = 0
@@ -68,6 +76,7 @@ class Game:
         onGreen = False
 
         def move(next_tile):
+            onGreen = False
 ############################################################################################################################################
             def slip(player_direction_y, player_direction_x):
                 self.Player.setFlavour(False)
@@ -97,7 +106,14 @@ class Game:
 
                     elif next_tile == [5]:
                         slip(player_direction_y, player_direction_x)
-            
+                    
+                    if (next_tile == [3]) and not (onGreen): #green tile score counter, outside move because onGreen needs to be stored
+                        self.score += 1
+                        print(self.score)
+                        onGreen = True 
+                    else:
+                        onGreen = False
+
         #main loops
         while True:
             #handle events
@@ -121,7 +137,7 @@ class Game:
                         player_direction_x = 1
                     elif event.key == pygame.K_LEFT:
                         player_direction_x = -1
-                    try:
+                    try: #only need to check next tile when a key is pressed
                         next_tile = self.check_tile(player_direction_x, player_direction_y)
                     except:
                         pass
@@ -132,30 +148,22 @@ class Game:
                         player_direction_x = 0
 
             #execute logic
-            #attempt to get the next tile, if no tile exists then pass to avoid crashing
-                
-                try:
-                    next_tile = self.check_tile(player_direction_x, player_direction_y)
-                except:
-                    pass
             move(next_tile)
-            if (next_tile == [3]) and not (onGreen): #green tile score counter, outside move because onGreen needs to be stored
-                self.score += 1
-                print(self.score)
-                onGreen = True 
-            else:
-                onGreen = False
 
             if next_tile == [0]:
                 #complete game, save any achievement data earned
+                print("you are win")
                 return
             
             next_tile = [1] #reset next tile
             #update display
             self.draw()
+            #print(f"{self.seconds} seconds")
+            self.seconds = pygame.time.get_ticks() / 1000
             clock.tick(10)
 
 if __name__ == "__main__":
-    game = Game(10,10,255)
+    game = Game(25,20,255)
     game.run_game_loop()
     print(game.mazeArray)
+
--- mazearray.py
+++ mazearray.py
@@ -18,10 +18,10 @@ class MazeArray:
         return self.grid
 
     def fillMaze(self):
-        for x in range(len(self.grid)):
-            for y in range(len(self.grid[x])):
+        for y in range(len(self.grid)):
+            for x in range(len(self.grid[y])):
                 if self.grid[y][x] == "#":
-                    self.setTile(x, y, self.chooseTile(False))
+                    self.setTile(x, y, self.chooseTile("filling"))
         self.grid = [x + [[0]] for x in self.grid] 
         #force 0 to generate as an array. this is because all the other numbers generate as lists. 
         #the reason for this is unknown however it doesn't affect much so i'd rather not spend 10 hours fixing this behaviour i only have about a month to get this done
@@ -33,32 +33,42 @@ class MazeArray:
         print(self.grid[y][x])
         return self.grid[y][x]
 
-    def chooseTile(self, safe):
-        if safe:
+    def chooseTile(self, mode):
+        if mode == "safe":
             return random.choices(range(1,6))
+            #return[1]
+        elif mode == "filling":
+            return random.choices(range(1,8), weights=(0.1, 0.1, 0.1, 0.1, 0.15, 0.2, 0.2))
+            #return [6] 
         else:
-            #return random.choices(range(1,8), weights=(0.1, 0.1, 0.15, 0.15, 0.15, 0.2, 0.2))
-            return [6] 
+            return random.choices([1, 3, 4, 5])
         #red/yellow is 3x more likely with "unsafe" generation, this is because red/yellow tiles are not generated during path generation.
 
     def generatePath(self):
         random.seed = self.seed
         lastTile = 0 #need to keep track of last tile. 
+        stillOrange = False
         #If a purple was placed, direction must be the same for one tile. If an orange tile was placed, do not use blue until purple is placed.
         #start halfway down the maze, at x = 0, because it is player spawn point
         y = round(((self.height)/2))
         x = 0
-        #print(x)
-        #print(y)
-        while x != (self.width): #+1 removed, thought it would end loop after filling, caused overflow, reverted
-            chosenTile = self.chooseTile(True) #pick a new tile
-            direction = 0
-            #print(x)
-            #print(y)
+        chosenTile = None
+        while x != (self.width):
             self.setTile(x, y, chosenTile)
             self.solpath.append([x, y, chosenTile])
-            axis = random.choice(["x", "x", "y"])   #2/3 chance of changing x
-            #print(axis)
+            if lastTile == [4] or stillOrange:
+                stillOrange = True
+                chosenTile = self.chooseTile("")
+                if chosenTile == [5]:
+                    stillOrange = False
+            else:
+                chosenTile = self.chooseTile("safe") #pick a new tile
+            if not lastTile == [5]: #only change axis when purple tile not changed
+                axis = random.choice(["x", "x", "y"])
+            elif lastTile == [2]:
+                for i in (-1, 1):
+                    self.setTile(x + i, y, self.chooseTile("safe"))
+                    self.setTile(x, y + i, self.chooseTile("safe"))
             if axis == "x":
                 direction = random.choice([-1, -1, 1, 1, 1])    #weighting the algorithm to the right to be less prone to walking into itself
                 if x == 0:
@@ -77,7 +87,7 @@ class MazeArray:
                     direction = 1
                 y += direction
                 #print(y)
-
+            lastTile = chosenTile
 
         #place a pink tile at player spawn, AFTER path generation
         self.setTile(0, round(((self.height)/2)), [1])
@@ -89,4 +99,5 @@ if __name__ == "__main__": #used for testing
     maze.fillMaze()
     for row in maze.grid:
         print(*row, sep="\t")
+    #print(maze.getTile(0,0))
     print(maze.solpath)
